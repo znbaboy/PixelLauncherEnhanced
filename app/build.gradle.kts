7f4356245bcb16b28e3164a9a0da0d06b8cef0e8
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2025-04-14 12:29:48 +0600

    Bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.drdisagree.pixellauncherenhanced"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
         setProperty("archivesBaseName", "PLEnhanced v${defaultConfig.versionName}")
     }
 
