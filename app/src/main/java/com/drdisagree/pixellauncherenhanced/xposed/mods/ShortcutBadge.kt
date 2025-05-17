e8cc77025cb3f87ee6e6b1381ad6886956a12d56
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2025-05-17 17:05:28 +0600

    Fix hiding shortcut icon badge

--- app/src/main/java/com/drdisagree/pixellauncherenhanced/xposed/mods/ShortcutBadge.kt
+++ app/src/main/java/com/drdisagree/pixellauncherenhanced/xposed/mods/ShortcutBadge.kt
@@ -1,11 +1,11 @@
 package com.drdisagree.pixellauncherenhanced.xposed.mods
 
 import android.content.Context
-import android.graphics.Path
 import com.drdisagree.pixellauncherenhanced.data.common.Constants.REMOVE_ICON_BADGE
 import com.drdisagree.pixellauncherenhanced.xposed.ModPack
 import com.drdisagree.pixellauncherenhanced.xposed.mods.LauncherUtils.Companion.reloadIcons
 import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.XposedHook.Companion.findClass
+import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.callMethod
 import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.hookConstructor
 import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.hookMethod
 import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.setField
@@ -33,47 +33,24 @@ class ShortcutBadge(context: Context) : ModPack(context) {
         bubbleTextViewClass
             .hookConstructor()
             .runAfter { param ->
-                if (removeBadge) {
+                if (!removeBadge) return@runAfter
                     param.thisObject.setField("mHideBadge", true)
-                }
             }
 
         bubbleTextViewClass
             .hookMethod("setHideBadge")
             .runAfter { param ->
-                if (removeBadge) {
+                if (!removeBadge) return@runAfter
                     param.thisObject.setField("mHideBadge", true)
-                }
             }
 
-        try {
-            bitmapInfoClass
-                .hookMethod("getBadgeDrawable")
-                .parameters(
-                    Context::class.java,
-                    Boolean::class.java,
-                    Boolean::class.java
-                )
-                .throwError()
-                .runBefore { param ->
-                    if (removeBadge) {
-                        param.result = null
-                    }
-                }
-        } catch (_: Throwable) {
-            bitmapInfoClass
-                .hookMethod("getBadgeDrawable")
-                .parameters(
-                    Context::class.java,
-                    Boolean::class.java,
-                    Boolean::class.java,
-                    Path::class.java
-                )
-                .runBefore { param ->
-                    if (removeBadge) {
-                        param.result = null
-                    }
-                }
-        }
+        bitmapInfoClass
+            .hookMethod("applyFlags")
+            .runAfter { param ->
+                if (!removeBadge) return@runAfter
+
+                val fastBitmapDrawable = param.args[1]
+                fastBitmapDrawable.callMethod("setBadge", null)
+            }
     }
 }
